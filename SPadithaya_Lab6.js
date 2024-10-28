691bb47ca22e464fe5b9efd8ef77aa3a015621ee
Sharanya Padithaya <spad@terpmail.umd.edu>
2024-10-27 21:13:13 -0400

    feat: new changes

--- SPadithaya_Lab6.js
+++ SPadithaya_Lab6.js
@@ -6,12 +6,30 @@ function createMap() {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-    var marker = L.marker([51.5, -0.09]).addTo(map);
+    const randomPoints = [
+        {lat: getRandomInRange(30,35,3), lng: getRandomInRange(-90,-100,3)},
+        {lat: getRandomInRange(30,35,3), lng: getRandomInRange(-90,-100,3)},
+        {lat: getRandomInRange(30,35,3), lng: getRandomInRange(-90,-100,3)},
+    ];
+    randomPoints.forEach((point,index) => {
+        const marker = L.marker([point.lat, point.lng]).addTo(map);
+
+        getLocality(point.lat, point.lng).then(locality => {
+            marker.getPopup().setContent(`Marker ${index + 1} - Locality: ${locality}`);
+        })
+    });
+
+
 }
 
-
-window.onload = createMap;
+function getLocality(lat, lng){
+    return fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`)
+        .then(response => response.json())
+        .then(data => data.locality || 'Unknown');
+}
 
 function getRandomInRange(from, to, fixed){
     return (Math.random() * (to - from) + from).toFixed(fixed)*1
 }
+
+window.onload = createMap;
